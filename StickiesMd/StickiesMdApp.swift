3e9365a771c8f7b5960354f07b835a881301b8b2
Ryohei Ueda <garaemon@gmail.com>
2026-01-21 22:27:21 -0800

    Initial Commit

---
+++ StickiesMd/StickiesMdApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickiesMdApp.swift
+//  StickiesMd
+//
+//  Created by Ryohei Ueda on 2026/01/21.
+//
+
+import SwiftUI
+
+@main
+struct StickiesMdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
